ca6eef8841da8b1957794616f5cb7bfddbbf2056
Clement Trouillard <clement.TROUILLARD@canal-plus.com>
2019-01-04 15:28:20 +0100

    boilerplate shit

---
+++ ratp.el
@@ -0,0 +1,52 @@
+;;; ratp.el ---
+;;
+;; Filename: ratp.el
+;; Description: RATP API Client for emacs
+;; Author: Clement Trosa <me@trosa.io>
+;; Maintainer: Clement Trosa <me@trosa.io>
+;; Created: ven. janv.  4 15:20:29 2019 (+0100)
+;; Version: 0.0.1
+;; Package-Requires: ()
+;; Last-Updated: ven. janv.  4 15:20:29 2019 (+0100)
+;;           By: Clement
+;;     Update #: 0
+;; URL: https://github.com/iomonad/ratp.el
+;; Doc URL: https://github.com/iomonad/ratp.el
+;; Keywords: ratp client
+;; Compatibility: 25
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;   No comment
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;   - 0.0.1: Project INIT
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(defun foo-bar ()
+  (print "hello world"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ratp.el ends here
